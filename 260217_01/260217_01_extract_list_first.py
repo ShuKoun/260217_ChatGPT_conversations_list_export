0fbcc500ae999b6f1fea9fa4e5e2609d1820afc5
shukoun <shukountokyo@gmail.com>
2026-02-17 23:18:58 +0900

    Update 260217_01_extract_list_first.py

--- 260217_01/260217_01_extract_list_first.py
+++ 260217_01/260217_01_extract_list_first.py
@@ -1,6 +1,6 @@
 import ijson
 
-INPUT_FILE = "conversations.json"
+INPUT_FILE = "/Users/user01/Library/Mobile Documents/com~apple~CloudDocs/20231225_inbox/260212_01_chatgpt01_backup_01/b33a994df128ffad992db1bfac6de93f0411465f4d13bfb4377bc6c0e9c6227f-2026-02-10-14-26-34-b7a4dedae62048369f9e6409c22f54e1/conversations.json"
 TMP_FILE = "conv_tmp.tsv"
 
 with open(INPUT_FILE, "rb") as f, open(TMP_FILE, "w", encoding="utf-8") as out:
